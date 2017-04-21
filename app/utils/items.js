31bdf3a7513f8d614e184bfff013bff4e21ca19e
Edin Skeja <edin.skeja@me.com>
2017-04-21 17:36:07 +0200

    New shit

--- app/utils/items.js
+++ app/utils/items.js
@@ -1,10 +1,42 @@
 export default [
 	{
 		emoji: '🍺',
-		label: 'Beer'
+		label: 'Beer',
+    points: 1,
 	},
 	{
-		emoji: '🚬',
-		label: 'Cigarette'
-	}
+		emoji: '🍹',
+		label: 'Drink',
+    points: 1,
+	},
+  {
+    emoji: '💦',
+    label: 'Shot',
+    points: 2,
+  },
+  {
+    emoji: '🚬',
+    label: 'Cigarette',
+    points: 2,
+  },
+  {
+    emoji: '🍁',
+    label: 'Joint',
+    points: 4,
+  },
+  {
+    emoji: '☠',
+    label: 'Space Cake',
+    points: 10,
+  },
+  {
+    emoji: '🚓',
+    label: 'Ran from the cops',
+    points: 450,
+  },
+  {
+    emoji: '🏥',
+    label: 'Cocaine',
+    points: 999,
+  },
 ];
