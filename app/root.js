b95c057f3321135cbd9fa984986a2ce9ca951c47
Dominic Lind <linddominic@gmail.com>
2017-04-21 17:21:18 +0200

    + codepush

--- app/root.js
+++ app/root.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import codePush from "react-native-code-push";
 
 import {
   View,
@@ -6,58 +7,22 @@ import {
   ActivityIndicator
 } from 'react-native';
 
-import { checkLogin } from 'app/utils/functions';
-
 // screens
-import Login from 'app/screens/Login';
-import Feed from 'app/screens/Feed';
+import Router from './router';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      loading: true,
-      route: false
-    }
-  }
-
-  componentDidMount() {
-    checkLogin(evt => {
-      this.setState({ loading: false });
-      if(evt.authenticated){
-        this.setState({route:'feed'});
-      } else {
-        this.setState({route:'login'});
-      }
-    });
-  }
-
   render() {
-
-
-    if(this.state.loading) {
-      return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <ActivityIndicator />
-        </View>
-      )
-    }else {
-      switch(this.state.route){
-        case 'login':
-          return <Login />
-        break;
-        case 'feed':
-          return <Feed />
-        break;
-
-        default:
-          return <Login />
-        break;
-      }
-    }
+     return (
+        <Router />
+      ) 
   }
 }
 
-export default App
+let codePushOptions = {
+  deploymentKey: 'mdmKRHH_r2aHa2sRy4CE9062OWL3VJKOj_Okb',
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+  updateDialog: true,
+  installMode: codePush.InstallMode.IMMEDIATE
+};
+
+export default codePush(codePushOptions)(App)
